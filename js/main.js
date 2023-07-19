28d2b05f526ce2e670469e5aa9859e27f2fcf318
CarloCassini <c.cassini87@gmail.com>
2023-07-19 16:42:49 +0200

    fixed NaN check

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 console.log("java ok wakka");
 
 // dichiarazione delle variabili
-let KmPercorso = parseInt(prompt("quanti kilometri farai?"));
+let KmPercorso = parseFloat(prompt("quanti kilometri farai?"));
 let Eta = parseInt(prompt("qual'è la tua età?"));
 let PrezzoViaggio;
 
@@ -11,7 +11,7 @@ PrezzoViaggio = KmPercorso * 0.21;
 
 console.log(PrezzoViaggio);
 
-if ((KmPercorso == NaN) | (Eta == NaN)) {
+if (isNaN(KmPercorso) || isNaN(Eta)) {
   console.log(" ricarica e inserisci valori numerici ");
 } else {
   if (Eta < 18) {
@@ -19,11 +19,11 @@ if ((KmPercorso == NaN) | (Eta == NaN)) {
   } else if (Eta >= 65) {
     PrezzoViaggio = PrezzoViaggio - (PrezzoViaggio * 40) / 100;
   }
+  console.log(PrezzoViaggio);
+
+  PrezzoViaggio = PrezzoViaggio.toFixed(2);
+
+  console.log(PrezzoViaggio);
+
+  document.getElementById("message-to-read").innerHTML = PrezzoViaggio;
 }
-console.log(PrezzoViaggio);
-
-PrezzoViaggio = PrezzoViaggio.toFixed(2);
-
-console.log(PrezzoViaggio);
-
-document.getElementById("message-to-read").innerHTML = PrezzoViaggio;
